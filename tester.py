87ad9a3417cc20be644e1ddf11f649b721abc30e
= <=>
2019-05-09 12:47:45 -0300

    aaaaa

--- tester.py
+++ tester.py
@@ -1,8 +1,4 @@
+# todo excluir o arquivo dps
 
 from datacore import core
 
-
-installer = core.Installer(file_name="datacore/database.json")
-
-dt = {"Package_nm": "Pycharm", "Command":"sudo snap install pycharm-commuinity --classic"}
-installer.add_package(dt)
